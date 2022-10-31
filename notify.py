a4d7484d8f4f061c0277f7cfd658784e1b5552c7
yunke120 <fj_18860352816@163.com>
2022-10-31 09:10:18 +0800

    update yml 0910

--- notify.py
+++ notify.py
@@ -12,7 +12,7 @@ class Notify:
         current_time = datetime.datetime.now().strftime('%Y.%m.%d-%H:%M:%S')
         self.content = {
             "------\n"
-            "### ZZU健康\n"
+            "### 打卡信息\n"
             "- 用户学号：" + str(username) + "\n"
             "- 打开状态：" + str(msg) + "\n"
             "- 打卡时间：" + current_time
@@ -22,7 +22,7 @@ class Notify:
         self.url = 'https://sctapi.ftqq.com/' + sckey + '.send'
         self.diy_content(username, msg)
         data = {
-            "text":"PFvpn每日签到",
+            "text":"ZZU每日健康打卡",
             "desp":self.content
         }
         requests.post(self.url, data=data, headers={'Content-type': 'application/x-www-form-urlencoded'})
